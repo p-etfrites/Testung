cf735a99e80b7858e9d6ba660ba7687366ee286e
A <arsehoefvk@gmail.com>
2024-12-17 18:18:41 +0000

    Add files via upload

---
+++ uu5-environment.js
@@ -0,0 +1,3 @@
+var uu5Environment = {"uu5loaderg01_initUuAppDisabled":false,"uuForum":{"hideUnauthorizedCommentPoint":true,"commentPointDefaultProfileMap":{"Authorities":["Authorities"],"Executives":["Writers"],"Reviewers":["Writers"],"TestReaders":["Writers"]}},"uu_plus4u5g02_uuApp":{"name":"uuBookKit","background":"#fff"},"uu_app_oidc_backend_available":true,"uu_app_oidc_backend_grant_call_token_available":true,"uu_app_oidc_providers_oidcg01_enabled":false};
+var UU5 = window.UU5 || {};
+UU5.Environment = uu5Environment;
